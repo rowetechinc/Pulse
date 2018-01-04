bd9b721b633b35551a4a66e4e66a0c1e51ef2205
rowetechinc <rcastelo@rowetechinc.com>
2018-01-04 10:25:52 -0800

    Updated the url to check for updates

--- Pulse/ViewModels/AboutViewModel.cs
+++ Pulse/ViewModels/AboutViewModel.cs
@@ -465,7 +465,7 @@ namespace RTI
         /// </summary>
         private void CheckForUpdates()
         {
-            string url = @"http://www.rowetechinc.com/pulse/Pulse_AppCast.xml";
+            string url = @"http://www.rowetechinc.co/pulse/Pulse_AppCast.xml";
 
             try
             {
