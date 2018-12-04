c199c03b25dcfc060f4221ab4decd661978c021d
rowetechinc <rcastelo@rowetechinc.com>
2018-12-04 15:30:13 -0800

    Updated version. Fixed issues in RTI library with PD0

--- Pulse/Properties/AssemblyInfo.cs
+++ Pulse/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the ranges or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.12.5.*")]
-[assembly: AssemblyFileVersion("4.12.5.0")]
+[assembly: AssemblyVersion("4.12.6.*")]
+[assembly: AssemblyFileVersion("4.12.6.0")]
