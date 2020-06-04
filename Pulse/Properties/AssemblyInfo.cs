f9154fbc312bf6a63789084b6a00db6791716820
Rico Castelo <rcastelo@rowetechinc.com>
2020-06-04 15:58:05 -0700

    Pulse-4.13.4   - Added options to turn on or off MarkBadBelowBottom and MarkBadAboveSurface for Amplitude and Correlation.

--- Pulse/Properties/AssemblyInfo.cs
+++ Pulse/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the ranges or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.13.3.*")]
-[assembly: AssemblyFileVersion("4.13.3.0")]
+[assembly: AssemblyVersion("4.13.4.*")]
+[assembly: AssemblyFileVersion("4.13.4.0")]
